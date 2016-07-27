29a166d4de351f135f607941708bd8df488c9835
jawahar <jawahar273@gmail.com>
2016-07-27 20:37:55 +0530

    Update crosswords.py

--- crosswords.py
+++ crosswords.py
@@ -1,1 +1,51 @@
 
+
+"""
+P     m
+ r   a
+  o r
+   g
+  o r
+ r   a
+P     m
+
+"""
+t = []
+
+p = input()#'Program'
+def inc(a):
+    r  = a%2
+    return a+r
+
+l = len(p)
+l2= inc(l//2)
+r = l-1
+
+print(p,"\n",r,"\nlen:",l,"\nhalf len",l2)
+
+for i in range(l):
+    t.append(list(" "*l))	
+
+print(len(t[0]))
+
+tr = 0
+
+for i in range(l2):
+ 
+     t[i][tr] = p[i]
+     t[i][r] =  p[r]
+     tr+=1
+     r-=1
+
+
+for i in range(l2,l):
+     t[i][tr] = p[i]
+     t[i][r] =  p[r] 
+     r-=1
+     tr+=1
+     
+for i in range(l):
+  for j in range(l):
+   print(t[i][j],end="")
+  print()
+
