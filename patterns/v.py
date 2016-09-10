75b42f6cd39bb3a0bdd3ad092cdbffef5447b248
jawahar <jawahar273@gmail.com>
2016-09-10 11:58:26 +0530

    Create v.py

---
+++ patterns/v.py
@@ -0,0 +1,23 @@
+limit = 6
+for i in range(0, limit):
+    print('#'+' '*i+'#')
+"""
+#      #
+#     #
+#    #
+#   #
+#  #
+# #
+
+"""
+for i in range(0, limit):
+    print('#'+' '*i+'#')
+ """  answer:
+ ##
+ # #
+ #  #
+ #   #
+ #    #
+ #     #
+
+ """
