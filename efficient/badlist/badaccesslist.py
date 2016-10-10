693c9c3ce590e3ffb7cbd098be38b354fdcadf74
jawahar <jawahar273@gmail.com>
2016-10-10 14:54:43 +0530

    Update badaccesslist.py

--- efficient/badlist/badaccesslist.py
+++ efficient/badlist/badaccesslist.py
@@ -1,7 +1,7 @@
 r = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 def run():
   for  i in range(len(r)):
-    print(i)
+    print(r[i])
 
 # timeit with print: 1000 loops, best of 3: 2.81 ms per loop
 # timeit without print: 1000000 loops, best of 3: 1.82 µs per loop
