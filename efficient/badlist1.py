bb60d51062f9fa93cedca445dc92deac99f7d2a7
jawahar <jawahar273@gmail.com>
2016-10-10 14:29:56 +0530

    Update badlist1.py

--- efficient/badlist1.py
+++ efficient/badlist1.py
@@ -1,7 +1,8 @@
 r = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 n = len(r)
-
+e = []
 def run():
   for i in range(n):
-     print(r[i])
-# apprx time : 1.12 ms with timeit
+     if i % 2 == 0:
+        e.append(i)
+# timeit 100000 loops, best of 3: 3.42 µs per loop
