a95fd8d5547c10a9197badb97ae9b11a31d2f3b9
jawahar <jawahar273@gmail.com>
2017-03-13 10:25:57 +0530

    Update good1.py

--- Pythonic/Iterators-and-Generators/good/good1.py
+++ Pythonic/Iterators-and-Generators/good/good1.py
@@ -1,1 +1,12 @@
 
+""""
+`map()` is an in-built function in Python 
+""""
+def sqrt_it(iter):
+  return map(lambda x:x**2, iter)
+
+import numpy as np
+
+seq = np.arange(10)
+for i in sqrt_it(seq):
+  print(i)
